caf13365c80b07e394332f1da06da7cf70d0514a
Mateusz Czaplinski <czapkofan@gmail.com>
2017-04-17 22:25:25 +0000

    math2svg: trivial stuff hand-fixed

--- math2svg.lua
+++ math2svg.lua
@@ -1,8 +1,6 @@
 -- Command-line tool to replace MathML with SVG throughout a document.
 -- 
 -- Replaces all instances of MathML throughout the document
-local getopt = require('getopt')
-local sys = require('sys')
 local os.path = require('os.path')
 local sax = require('xml').sax
 local XMLGenerator = require('svgmath.tools.saxtools').XMLGenerator
@@ -20,13 +18,14 @@ open_or_die = function(fname, fmode, role)
 end
 
 usage = function()
-  sys.stderr.write [[
-Usage: math2svg.lua [options] FILE
-Replaces MathML formulae in a document by SVG images. Argument is a file name.
+  io.stderr:write [[
+Usage: math2svg.lua [options]
+Replaces MathML formulae in a document by SVG images.
 
 Options:
     -h, --help               display this synopsis and exit
     -s, --standalone         treat input as a standalone MathML document
+    -i=FILE, --input=FILE    read MathML input from FILE instead of stdin
     -o=FILE, --output=FILE   write results to FILE instead of stdout
     -c=FILE, --config=FILE   read configuration from FILE
     -e=ENC,  --encoding=ENC  produce output in ENC encoding
@@ -72,71 +71,67 @@ MathFilter = PYLUA.class(ContentFilter) {
   end;
 }
 
+local function flag_get(old, s, ...)
+  if not s then return old end
+  for _, prefix in ipairs{...} do
+    if s:sub(1,#prefix+1) == prefix..'=' then
+      return s:sub(#prefix+2)
+    end
+  end
+  return old, s
+end
 
-main = function()
-  -- PYLUA.FIXME: TRY:
-    opts, args = getopt.getopt(PYLUA.slice(sys.argv, 1, nil), 'c:e:ho:s', {'config=', 'encoding=', 'help', 'output=', 'standalone'})
-  -- PYLUA.FIXME: EXCEPT getopt.GetoptError:
-    usage()
-    sys.exit(2)
 
-  outputfile = nil
-  configfile = nil
-  encoding = 'utf-8'
-  standalone = false
+main = function(...)
+  local inputfile = io.stdin
+  local outputfile = io.stdout
+  local configfile = nil
+  local encoding = 'utf-8'
+  local standalone = false
 
-  for _, o, a in ipairs(opts) do
-    if PYLUA.op_in(o, '-h', '--help') then
+  for _, a in ipairs(...) do
+    if a=='-h' or a=='--help' then
       usage()
-      sys.exit(0)
+      os.exit(2)
     end
-    if PYLUA.op_in(o, '-o', '--output') then
-      outputfile = a
-    end
-    if PYLUA.op_in(o, '-c', '--config') then
-      configfile = a
-    end
-    if PYLUA.op_in(o, '-e', '--encoding') then
-      encoding = a
-    end
-    if PYLUA.op_in(o, '-s', '--standalone') then
+    inputfile, a = flag_get(inputfile, a, '-i', '--input')
+    outputfile, a = flag_get(outputfile, a, '-o', '--output')
+    configfile, a = flag_get(configfile, a, '-c', '--config')
+    encoding, a = flag_get(encoding, a, '-e', '--encoding')
+    if a=='-s' or a=='--standalone' then
       standalone = true
+    elseif a then
+      error('unknown flag: '..a)
     end
   end
 
-  -- Check input
-  if len(args)<1 then
-    sys.stderr.write('No input file specified!\n')
-    usage()
-    sys.exit(1)
-  elseif len(args)>1 then
-    sys.stderr.write('WARNING: extra command line arguments ignored\n')
+  local source = inputfile
+  if type(source)=='string' then
+    source = open_or_die(source, 'rb', 'input')
   end
-
-  source = open_or_die(args[1], 'rb', 'input')
 
   -- Determine output destination
-  if PYLUA.op_is(outputfile, nil) then
-    output = sys.stdout
-  else
-    output = open_or_die(outputfile, 'wb', 'output')
+  local output = outputfile
+  if type(output)=='string' then
+    output = open_or_die(output, 'wb', 'output')
   end
 
   -- Determine config file location
-  if PYLUA.op_is(configfile, nil) then
+  if not configfile then
+    -- FIXME
     configfile = PYLUA.str_maybe(os.path).join(os.path.dirname(__file__), 'svgmath.xml')
   end
-  config = open_or_die(configfile, 'rb', 'configuration')
+  local config = open_or_die(configfile, 'rb', 'configuration')
 
   -- Create the converter as a content handler. 
-  saxoutput = XMLGenerator(output, encoding)
-  handler = MathHandler(saxoutput, config)
-  if  not standalone then
+  local saxoutput = XMLGenerator(output, encoding)
+  local handler = MathHandler(saxoutput, config)
+  if not standalone then
     handler = MathFilter(saxoutput, handler)
   end
 
   -- Parse input file
-  exitcode = 0
+  local exitcode = 0
   -- PYLUA.FIXME: TRY:
     parser = sax.make_parser()
     parser.setFeature(sax.handler.feature_namespaces, 1)
@@ -144,15 +139,14 @@ main = function()
     parser.setContentHandler(handler)
     parser.parse(source)
   -- PYLUA.FIXME: EXCEPT sax.SAXException xcpt:
-    io.write(PYLUA.mod('Error parsing input file %s: %s', args[1], xcpt.getMessage()), '\n')
+    print(string.format('Error parsing input file %s: %s', args[1], xcpt.getMessage()))
     exitcode = 1
-  source.close()
+  source:close()
   if PYLUA.op_is_not(outputfile, nil) then
-    output.close()
+    output:close()
   end
-  sys.exit(exitcode)
+  os.exit(exitcode)
 end
 
-if __name__=='__main__' then
-  main()
-end
+-- TODO: detect somehow if we're require()d or run from command-line
+main(...)
