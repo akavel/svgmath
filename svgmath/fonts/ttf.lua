07d9a4ae90c8d7c5dbe857b9f41bf87054b27478
Mateusz Czaplinski <czapkofan@gmail.com>
2017-04-21 22:51:39 +0000

    fonts/ttf.lua: hand-tweak

--- svgmath/fonts/ttf.lua
+++ svgmath/fonts/ttf.lua
@@ -58,17 +58,20 @@ TTFMetric = PYLUA.class(FontMetric) {
 
   readFontMetrics = function(self, ff)
     local version = ff:read(4)
-    if PYLUA.map(ord, version)=={0, 1, 0, 0} then
+    if version == '\x00\x01\x00\x00' then
       self.fonttype = 'TTF'
     elseif version=='OTTO' then
+      -- self.fonttype="OTF"
+      -- At the moment, I cannot parse bbox data out from CFF
       error(TTFFormatError('OpenType/CFF fonts are unsupported'))
     else
       error(TTFFormatError('Not a TrueType file'))
     end
+
     local numTables = readUnsigned(ff, 2)
     local tables = { }
     skip(ff, 6)
-    for _, i in ipairs(range(0, numTables)) do
+    for i = 1,#numTables do
       local tag = ff:read(4)
       local checksum = readUnsigned(ff, 4)
       local offset = readUnsigned(ff, 4)
@@ -76,75 +79,85 @@ TTFMetric = PYLUA.class(FontMetric) {
       tables[tag] = {offset, length}
     end
 
-    switchTable = function(tableTag)
-      if PYLUA.op_not_in(tableTag, PYLUA.keys(tables)) then
-        error(TTFFormatError('Required table '+tableTag+' missing in TrueType file'))
-      end
-      return tables[tableTag]
+    local switchTable = function(tableTag)
+      return tables[tableTag] or 
+        error(TTFFormatError('Required table '..tableTag..' missing in TrueType file'))
     end
+
     local offset, length = table.unpack(switchTable('head'))
     ff:seek(offset+12)
     local magic = readUnsigned(ff, 4)
-    if magic~=1594834165 then
+    if magic~=0x5F0F3CF5 then
       error(TTFFormatError('Magic number in \'head\' table does not match the spec'))
     end
     skip(ff, 2)
     self.unitsPerEm = readUnsigned(ff, 2)
     local emScale = 1.0/self.unitsPerEm
+
     skip(ff, 16)
     local xMin = readSigned(ff, 2)*emScale
     local yMin = readSigned(ff, 2)*emScale
     local xMax = readSigned(ff, 2)*emScale
     local yMax = readSigned(ff, 2)*emScale
     self.bbox = {xMin, yMin, xMax, yMax}
+
     skip(ff, 6)
     self.indexToLocFormat = readSigned(ff, 2)
+
     offset, length = table.unpack(switchTable('maxp'))
     ff:seek(offset+4)
     self.numGlyphs = readUnsigned(ff, 2)
+
     offset, length = table.unpack(switchTable('name'))
     ff:seek(offset+2)
+
     local numRecords = readUnsigned(ff, 2)
     local storageOffset = readUnsigned(ff, 2)+offset
+
     local uniNames = { }
     local macNames = { }
     local englishCodes = {1033, 2057, 3081, 4105, 5129, 6153}
-    for _, i in ipairs(range(0, numRecords)) do
+
+    for i = 1,#numRecords do
       local platformID = readUnsigned(ff, 2)
       local encodingID = readUnsigned(ff, 2)
       local languageID = readUnsigned(ff, 2)
       local nameID = readUnsigned(ff, 2)
       local nameLength = readUnsigned(ff, 2)
       local nameOffset = readUnsigned(ff, 2)
+
       if platformID==3 and encodingID==1 then
-        if PYLUA.op_in(languageID, englishCodes) or PYLUA.op_not_in(nameID, PYLUA.keys(uniNames)) then
+        if PYLUA.op_in(languageID, englishCodes) or not uniNames[nameID] then
           uniNames[nameID] = {nameOffset, nameLength}
         end
       elseif platformID==1 and encodingID==0 then
-        if languageID==0 or PYLUA.op_not_in(nameID, PYLUA.keys(macNames)) then
+        if languageID==0 or not macNames[nameID] then
           macNames[nameID] = {nameOffset, nameLength}
         end
       end
     end
 
-    getName = function(code)
-      if PYLUA.op_in(code, PYLUA.keys(macNames)) then
+    local getName = function(code)
+      if macNames[code] then
         local nameOffset, nameLength = table.unpack(macNames[code])
         ff:seek(storageOffset+nameOffset)
         return ff:read(nameLength)
-      elseif PYLUA.op_in(code, PYLUA.keys(uniNames)) then
+        -- FIXME(grigoriev): repair Mac encoding here
+      elseif uniNames[code] then
         nameOffset, nameLength = table.unpack(uniNames[code])
         ff:seek(storageOffset+nameOffset)
         local result = ''
-        for _, i in ipairs(range(0, nameLength/2)) do
+        for i = 1,nameLength/2 do
           result = result+unichr(readUnsigned(ff, 2))
         end
         return result
       end
     end
+
     self.family = getName(1)
     self.fullname = getName(4)
     self.fontname = getName(6)
+
     offset, length = table.unpack(switchTable('OS/2'))
     ff:seek(offset)
     local tableVersion = readUnsigned(ff, 2)
@@ -152,6 +165,7 @@ TTFMetric = PYLUA.class(FontMetric) {
     if cw then
       self.charwidth = cw*emScale
     end
+
     local wght = readUnsigned(ff, 2)
     if wght<150 then
       self.weight = 'Thin'
@@ -172,9 +186,11 @@ TTFMetric = PYLUA.class(FontMetric) {
     else
       self.weight = 'Black'
     end
+
     skip(ff, 62)
     self.ascender = readSigned(ff, 2)*emScale
     self.descender = readSigned(ff, 2)*emScale
+
     if tableVersion==2 then
       skip(ff, 14)
       local xh = readSigned(ff, 2)
@@ -186,36 +202,42 @@ TTFMetric = PYLUA.class(FontMetric) {
         self.capheight = ch*emScale
       end
     end
+
     offset, length = table.unpack(switchTable('post'))
     ff:seek(offset+4)
     self.italicangle = readFixed32(ff)
     self.underlineposition = readSigned(ff, 2)*emScale
     self.underlinethickness = readSigned(ff, 2)*emScale
+
     offset, length = table.unpack(switchTable('hhea'))
     ff:seek(offset+34)
     local numHmtx = readUnsigned(ff, 2)
+
     offset, length = table.unpack(switchTable('hmtx'))
     ff:seek(offset)
     local glyphArray = {}
     local w = 0
-    for _, i in ipairs(range(0, self.numGlyphs)) do
+    for i = 1,self.numGlyphs do
       if i<numHmtx then
         w = readUnsigned(ff, 2)*emScale
         skip(ff, 2)
       end
       table.insert(glyphArray, CharMetric(PYLUA.keywords{width=w}))
     end
+
     offset, length = table.unpack(switchTable('cmap'))
     ff:seek(offset+2)
     local subtableOffset = 0
     numTables = readUnsigned(ff, 2)
+
     local cmapEncodings = { }
-    for _, i in ipairs(range(0, numTables)) do
+    for i = 1,numTables do
       local platformID = readUnsigned(ff, 2)
       local encodingID = readUnsigned(ff, 2)
       subtableOffset = readUnsigned(ff, 4)
       cmapEncodings[PYLUA.keytuple{platformID, encodingID}] = subtableOffset
     end
+
     local encodingScheme = 'Unicode'
     subtableOffset = cmapEncodings[PYLUA.keytuple{3, 1}]
     if subtableOffset == nil then
@@ -227,43 +249,53 @@ TTFMetric = PYLUA.class(FontMetric) {
         self.log:write(string.format('WARNING: font \'%s\' is a symbolic font - Unicode mapping may be unreliable\n', self.fullname))
       end
     end
+
     ff:seek(offset+subtableOffset)
+
     local tableFormat = readUnsigned(ff, 2)
     if tableFormat~=4 then
       error(TTFFormatError(string.format('Unsupported format in \'cmap\' table: %d', tableFormat)))
     end
+
     local subtableLength = readUnsigned(ff, 2)
     skip(ff, 2)
     local segCount = readUnsigned(ff, 2)/2
     skip(ff, 6)
+
     local endCounts = {}
     for _, i in ipairs(range(0, segCount)) do
       table.insert(endCounts, readUnsigned(ff, 2))
     end
+
     skip(ff, 2)
     local startCounts = {}
-    for _, i in ipairs(range(0, segCount)) do
+    for i = 1,segCount do
       table.insert(startCounts, readUnsigned(ff, 2))
     end
+
     local idDeltas = {}
-    for _, i in ipairs(range(0, segCount)) do
+    for i = 1,segCount do
       table.insert(idDeltas, readSigned(ff, 2))
     end
+
     local rangeOffsets = {}
     for _, i in ipairs(range(0, segCount)) do
       table.insert(rangeOffsets, readUnsigned(ff, 2))
     end
+
     local remainingLength = subtableLength-8*segCount-16
     if remainingLength<=0 then
-      remainingLength = remainingLength+65536
+      remainingLength = remainingLength+65536  -- protection against Adobe's bug
     end
+
     local glyphIdArray = {}
-    for _, i in ipairs(range(0, remainingLength/2)) do
+    for i = 1,remainingLength/2 do
       table.insert(glyphIdArray, readUnsigned(ff, 2))
     end
-    for _, i in ipairs(range(0, segCount)) do
+
+    for i = 1,segCount do
       for _, c in ipairs(range(startCounts[i], endCounts[i]+1)) do
-        if c==65535 then
+        if c==0xFFFF then
           goto continue
         end
         local gid = 0
@@ -278,8 +310,10 @@ TTFMetric = PYLUA.class(FontMetric) {
         elseif gid<0 then
           gid = gid+65536
         end
+
         local cm = glyphArray[gid]
         table.insert(cm.codes, c)
+        -- Dirty hack: map the lower half of the private-use area to ASCII
         if encodingScheme=='Symbol' and PYLUA.op_in(c, range(61472, 61567)) then
           table.insert(cm.codes, c-61440)
         end
@@ -289,32 +323,35 @@ TTFMetric = PYLUA.class(FontMetric) {
         ::continue::
       end
     end
+
     offset, length = table.unpack(switchTable('loca'))
     ff:seek(offset)
     local glyphIndex = {}
     local scalefactor = self.indexToLocFormat+1
+
     if self.indexToLocFormat==0 then
-      for _, i in ipairs(range(0, self.numGlyphs+1)) do
+      for i = 1,self.numGlyphs+1 do
         table.insert(glyphIndex, readUnsigned(ff, 2)*2)
       end
     elseif self.indexToLocFormat==1 then
-      for _, i in ipairs(range(0, self.numGlyphs+1)) do
+      for i = 1,self.numGlyphs+1 do
         table.insert(glyphIndex, readUnsigned(ff, 4))
       end
     else
       error(TTFFormatError(string.format('Invalid indexToLocFormat value (%d) in \'head\' table', tostring(self.indexToLocFormat))))
     end
+
     offset, length = table.unpack(switchTable('glyf'))
-    for _, i in ipairs(range(0, self.numGlyphs)) do
+    for i = 1,self.numGlyphs do
       local cm = glyphArray[i]
       if glyphIndex[i]==glyphIndex[i+1] then
-        cm.bbox = {0, 0, 0, 0}
+        cm.bbox = {0, 0, 0, 0}  -- empty glyph
       else
         ff:seek(offset+glyphIndex[i]+2)
-        xMin = readSigned(ff, 2)*emScale
-        yMin = readSigned(ff, 2)*emScale
-        xMax = readSigned(ff, 2)*emScale
-        yMax = readSigned(ff, 2)*emScale
+        local xMin = readSigned(ff, 2)*emScale
+        local yMin = readSigned(ff, 2)*emScale
+        local xMax = readSigned(ff, 2)*emScale
+        local yMax = readSigned(ff, 2)*emScale
         cm.bbox = {xMin, yMin, xMax, yMax}
       end
       for _, c in ipairs(cm.codes) do
@@ -333,6 +370,7 @@ main = function()
     PYLUA.print('Usage: TTF.py <path to TTF file>', '\n')
   end
 end
+
 if __name__=='__main__' then
   main()
 end
